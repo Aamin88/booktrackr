5d3de679d17afb7c6510e8f7417a83b395bde7eb
Aamin88 <forkahamin@yahoo.co.uk>
2024-08-14 22:25:36 +0100

    updated: updating the API url to the live link

--- frontend/src/config/axios.js
+++ frontend/src/config/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
-const baseURL = "http://localhost:5002";
-// const baseURL = "https://booktrackr-api.vercel.app/books";
+// const baseURL = "http://localhost:5002";
+const baseURL = "https://booktrackr-api.vercel.app";
 
 // TODO: create another instances of axios of the get book route due
 // to GEMINI longer to generate text, give it a time of 1.2 minutes
